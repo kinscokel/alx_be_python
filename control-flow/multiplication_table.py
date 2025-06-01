bebe01c6406df81056d80673bbebdf69d62735ce
kingsley <kinscoco9@gmail.com>
2025-06-01 10:50:06 +0100

    committing my file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,10 +1,10 @@
 # multiplication table using for loop
 
 # prompt the user to enter a number
-number = int(input{"Enter a number to see its multiplication table: "})
+number = int(input("Enter a number to see its multiplication table "))
 
-# generate and print the multiplication table from 1 to 10
+# generate and print the multiplication table of the number to iterate through the numbers 1 to 10
 
-for i in range("1, 10"):
-    result = number * if
-    print(f"{number} * {i} = {result}")
+for i in range(1, 10):
+    product = number * i
+    print(f"{"number"} * {i} = {product}")
