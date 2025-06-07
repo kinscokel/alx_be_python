9ae5761f2145d95aa07c958aec0a049087f2f903
kingsley <kinscoco9@gmail.com>
2025-06-07 16:58:14 +0100

    committing a file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,19 +15,19 @@ def main():
             item = input("Enter the item to add:")
             if item:
                 shopping_list.append(item)
-                print("{item} has been added to your shopping list.")
+                print(f'"{item} has been added to your shopping list.')
             else:
                 print("item can not be empty.")
         elif choice == '2':
-            item = input('Enter the item to be removed':)
+            item = input("Enter the item to be removed:")
             if item in shopping_list:
                 shopping_list.remove(item)
-                print("{item} has been removed from your shopping list. ")
+                print(f'"{item} has been removed from your shopping list. ')
             else:
-                print("item is not in your shopping list.")
+                print(f'"{item}" is not in your shopping list.')
         elif choice == '3':
             if shopping_list:
-                print("items in your shopping list")
+                print(f'"{item}" in your shopping list')
         elif choice == '4':
             print("Goodbye!")
             break
