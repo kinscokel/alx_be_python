95c5591117e52d47b403ea86f83e324168e368df
kingsley <kinscoco9@gmail.com>
2025-06-16 16:57:18 +0100

    committing my file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,20 @@
 # Simple Bank Account Class
-
 class BankAccount:
-    def __init__(self, initial_balance=0):
+    def __init__(self, initial_balance=0.0):
         self.__account_balance = initial_balance
 
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
 
     def withdraw(self, amount):
-        if 0 <= amount <= self.__account_balance:
+        if amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
-        return False
+        else:
+            return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current balance: ${self.__account_balance:.2f}")
