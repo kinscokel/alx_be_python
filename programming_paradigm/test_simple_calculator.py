81f229966569641c8410ec17b769a44e27379f41
kingsley <kinscoco9@gmail.com>
2025-06-17 03:07:48 +0100

    committing my file

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,46 @@
+# test_simple_calculator.py
+
+
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up a calculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    # Test addition
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    # Test subtraction
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-3, -3), 0)
+
+    # Test multiplication
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-4, 5), -20)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+
+    # Test division
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertIsNone(self.calc.divide(5, 0))  # Edge case: division by zero
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+        
+        
+
+    
