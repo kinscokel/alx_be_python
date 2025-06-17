58b03922feac3758d3c8c8142b82d0a4dd86c2c3
kingsley <kinscoco9@gmail.com>
2025-06-17 03:12:23 +0100

    committing my file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,7 +11,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     # Test addition
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-1, 1), 0)
