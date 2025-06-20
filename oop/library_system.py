4c93fff9408e15c477614c62b080744c2d348fcc
kingsley <kinscoco9@gmail.com>
2025-06-20 17:28:48 +0100

    committing my file

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,27 +1,21 @@
-# library_system.py
+# # library_system.py
 
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_details(self):
-        return f"Title: {self.title}, Author: {self.author}"
-
     def __str__(self):
-        return self.get_details()
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self, title, author, file_size_kb):
         super().__init__(title, author)
-        self.file_size = file_size  # in MB
-
-    def get_details(self):
-        return f"{super().get_details()}, File Size: {self.file_size}MB"
+        self.file_size_kb = file_size_kb  # in KB
 
     def __str__(self):
-        return self.get_details()
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size_kb}KB"
 
 
 class PrintBook(Book):
@@ -29,11 +23,8 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
-        return f"{super().get_details()}, Page Count: {self.page_count}"
-
     def __str__(self):
-        return self.get_details()
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -44,11 +35,11 @@ class Library:
         if isinstance(book, Book):
             self.books.append(book)
         else:
-            raise TypeError("Only instances of Book or its subclasses can be added.")
+            raise TypeError("Only Book or its subclasses can be added.")
 
     def list_books(self):
         if not self.books:
             print("The library has no books.")
         else:
-            for idx, book in enumerate(self.books, 1):
-                print(f"{idx}. {book}")  # __str__ is called here
+            for book in self.books:
+                print(book)
